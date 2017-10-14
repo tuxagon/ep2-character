0e8e8cf4249e344d998b11c0a520ae3ff4afaf52
Kenneth Bogner <bognerkenny@gmail.com>
2017-10-14 13:39:43 -0400

    Added basic setup

---
+++ src/Main.elm
@@ -0,0 +1,39 @@
+module Main exposing (..)
+
+import Html exposing (..)
+
+
+type alias Model =
+    String
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( "", Cmd.none )
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ text model ]
+
+
+main : Program Never Model Msg
+main =
+    Html.program
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = \_ -> Sub.none
+        }
