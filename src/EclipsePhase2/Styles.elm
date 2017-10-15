c6e02538d6ef2c8f90cff843a4a47d52206762b4
Kenneth Bogner <bognerkenny@gmail.com>
2017-10-15 16:03:52 -0400

    Added Border style

--- src/EclipsePhase2/Styles.elm
+++ src/EclipsePhase2/Styles.elm
@@ -1,14 +1,20 @@
 module EclipsePhase2.Styles exposing (..)
 
 import Style exposing (StyleSheet, style)
+import Style.Border as Border
 
 
 type Styles
     = None
+    | Border
 
 
 stylesheet : StyleSheet Styles variation
 stylesheet =
     Style.styleSheet
         [ style None []
+        , style Border
+            [ Border.solid
+            , Border.all 1
+            ]
         ]
